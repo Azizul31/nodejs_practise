8c175715ff4e0e4cf6632ac02cd6cbb94a946166
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2018-12-25 03:41:29 +0600

    Add all files

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var vd=require('../videodata.json');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express',
+  name:'My favourite band',
+  videodata:vd });
+});
+
+module.exports = router;
