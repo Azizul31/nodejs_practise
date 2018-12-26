bcf3530b7824f669e0254ca4359c216e1842c276
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2018-12-26 23:31:38 +0600

    field related code

--- nodejsquery/demo_db_connections.js
+++ nodejsquery/demo_db_connections.js
@@ -9,13 +9,9 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
-    var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-    con.query(sql,function(err,result)
-
-    {
-        if(err) throw err;
-        console.log("1 row inserted");
+    con.query("SELECT name, address FROM customers", function (err, result, fields) {
+        if (err) throw err;
+        console.log(fields);
     });
 });
 
